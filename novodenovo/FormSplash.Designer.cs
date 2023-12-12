a7e9de101206bc2a746dcc4ea7557cc8d9a5ad86
VINICIUS GAMA <vinicius.gama9@etec.sp.gov.br>
2023-12-12 14:17:31 -0300

    Alteração na imagem do splash

--- novodenovo/FormSplash.Designer.cs
+++ novodenovo/FormSplash.Designer.cs
@@ -40,9 +40,10 @@ namespace novodenovo
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-240, -96);
+            this.pictureBox1.Location = new System.Drawing.Point(-196, -158);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(910, 624);
+            this.pictureBox1.Size = new System.Drawing.Size(1065, 818);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
